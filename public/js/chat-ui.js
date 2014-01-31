545590798f1143c1a3d64f978856dbd60d8f0ec7
Smitha Milli <smitha.milli@gmail.com>
2014-01-30 21:11:43 -0800

    Nickname jquery

--- public/js/chat-ui.js
+++ public/js/chat-ui.js
@@ -1,3 +1,8 @@
 $(function(){
 	var socket = io.connect();
+	$('#choose-nickname').submit(function(e){
+		e.preventDefault();
+		var nick = $('#nickname').val();
+		socket.emit('nickname', nick);
+	});
 });
