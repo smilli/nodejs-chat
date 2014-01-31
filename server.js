1e9db07baba4893d5e8444a1ad0a33ee7ea50c61
Smitha Milli <smitha.milli@gmail.com>
2014-01-30 20:56:44 -0800

    more chat server

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var http = require('http'),
 	chatServer = require('./lib/chat-server.js');
 
 var app = express();
-http.createServer(app).listen('3000', '127.0.0.1');
+var server = http.createServer(app).listen('3000', '127.0.0.1');
 
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
 	res.sendfile(__dirname + '/views/index.html');
 });
+
+chatServer.listen(server);
