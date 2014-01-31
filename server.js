9d232f6ae3d48d1000aa8d03d068df69119529b0
Smitha Milli <smitha.milli@gmail.com>
2014-01-30 20:48:24 -0800

    Add express middleware

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var http = require('http'),
 var app = express();
 http.createServer(app).listen('3000', '127.0.0.1');
 
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
 	res.sendfile(__dirname + '/views/index.html');
 });
